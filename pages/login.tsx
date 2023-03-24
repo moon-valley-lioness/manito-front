cc30504a749fc05fcc27318c94d59b624b0dbb61
kim-hyunjin <hyunjin1612@gmail.com>
2023-03-24 17:55:22 +0900

    login page css

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,6 +4,9 @@ import { FormEventHandler, useCallback } from 'react';
 import { setAuthToken } from '@/auth/lib/cookie';
 import { fetchAuthToken } from '@/auth/lib/fetch';
 
+const inputCss = 'rounded border p-1';
+const commonBtnCss = 'rounded text-white p-1 font-semibold';
+
 export default function Login() {
   const router = useRouter();
   const { id, pwd, isAutoLogin, handleIdInput, handlePwdInput, handleAutoLoginCheckbox } =
@@ -28,20 +31,37 @@ export default function Login() {
   }, [router]);
 
   return (
-    <main>
-      <form onSubmit={handleLogin}>
-        <input type='text' placeholder='아이디' value={id} onChange={handleIdInput} />
-        <input type='password' placeholder='비밀번호' value={pwd} onChange={handlePwdInput} />
+    <main className='w-96 m-auto flex flex-col mt-20'>
+      <form className='flex flex-col gap-2  mb-2' onSubmit={handleLogin}>
         <input
-          type='checkbox'
-          id='auto-login'
-          checked={isAutoLogin}
-          onChange={handleAutoLoginCheckbox}
+          className={inputCss}
+          type='text'
+          placeholder='아이디'
+          value={id}
+          onChange={handleIdInput}
         />
-        <label htmlFor='auto-login'>자동로그인</label>
-        <button>로그인</button>
+        <input
+          className={inputCss}
+          type='password'
+          placeholder='비밀번호'
+          value={pwd}
+          onChange={handlePwdInput}
+        />
+        <div>
+          <input
+            className='mr-1'
+            type='checkbox'
+            id='auto-login'
+            checked={isAutoLogin}
+            onChange={handleAutoLoginCheckbox}
+          />
+          <label htmlFor='auto-login'>자동로그인</label>
+        </div>
+        <button className={`${commonBtnCss} bg-sky-500 hover:bg-sky-400`}>로그인</button>
       </form>
-      <button onClick={handleRegisterBtn}>회원가입</button>
+      <button className={`${commonBtnCss} bg-red-700 hover:bg-red-600`} onClick={handleRegisterBtn}>
+        회원가입
+      </button>
     </main>
   );
 }
