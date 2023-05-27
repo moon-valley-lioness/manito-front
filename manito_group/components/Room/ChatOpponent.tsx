b888207135328f58c7566f6c64ff55b58fd73be6
kim-hyunjin <hyunjin1612@gmail.com>
2023-05-27 15:41:40 +0900

    change display chat opponent

--- manito_group/components/Room/ChatOpponent.tsx
+++ manito_group/components/Room/ChatOpponent.tsx
@@ -21,8 +21,6 @@ export default function ChatOpponent({
     }
   }, [chatId, activeChatId, setIsReaded]);
 
-  let displayName = opponentName;
-  if (!displayName) displayName = type == 'manito' ? '내가 도와주는 사람' : '나를 도와주는 사람';
   return (
     <li
       onClick={() => setActiveChatId(chatId)}
@@ -30,7 +28,10 @@ export default function ChatOpponent({
         activeChatId === chatId && 'bg-sky-500 text-white'
       } px-5 h-14 border-b-2 flex justify-center items-center font-bold hover:bg-gray-400 hover:text-white hover:cursor-pointer`}
     >
-      <div className='w-2/3 text-center'>{displayName}</div>
+      <div className='w-2/3 text-center'>
+        <span>{type == 'manito' ? '내가 도와주는 사람' : '나를 도와주는 사람'}</span>
+        {opponentName ? <span> ({opponentName})</span> : null}
+      </div>
       <div className='w-1/3 flex justify-end'>
         {newChatIncoming[chatId] > 0 && (
           <div className='w-6 h-6 bg-red-400 rounded-full text-white flex justify-center items-center'>
